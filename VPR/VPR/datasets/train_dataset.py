3fe836cbe045856a29f23510e72d58cf79f05f9a
AndreaG12 <80697707+AndreaG12@users.noreply.github.com>
2023-05-28 18:07:09 +0200

    Update train_dataset.py

--- VPR/VPR/datasets/train_dataset.py
+++ VPR/VPR/datasets/train_dataset.py
@@ -64,11 +64,9 @@ class TrainDataset(Dataset):
             image = Image.open(chosen_paths[0]).convert('RGB')
             
             img = self.transform(image)
-            print(type(img))
-            print("Lunghezza img: ", len(img))
-            print("lunghezza index:" , len(index))
             
-            return img, torch.tensor(index)   
+            
+            return img, torch.tensor(index).repeat(2)   #number of final augmented images
         else:
             images = [Image.open(path).convert('RGB') for path in chosen_paths]
             images = [self.transform(img) for img in images]
