6e7b4dfa6ebdd2dba9c550552ef8dfeedfe4d779
AndreaG12 <80697707+AndreaG12@users.noreply.github.com>
2023-05-29 12:23:54 +0200

    Update download_datasets.py

--- VPR/VPR/download_datasets.py
+++ VPR/VPR/download_datasets.py
@@ -1,15 +1,21 @@
 #Mio
-URLS = {
-    "tokyo_xs": "https://drive.google.com/file/d/15QB3VNKj93027UAQWv7pzFQO1JDCdZj2/view?usp=share_link",
-    "sf_xs": "https://drive.google.com/file/d/10ZVF3BzNNmNfDX5oPce_7SUZNwijqpvQ/view?usp=sharing",
-    "gsv_xs": "https://drive.google.com/file/d/1ozI-r1V5sTvFaA-7UBIyfk0WvKfH0NRL/view?usp=sharing"
-}
+#URLS = {
+ #   "tokyo_xs": "https://drive.google.com/file/d/15QB3VNKj93027UAQWv7pzFQO1JDCdZj2/view?usp=share_link",
+  #  "sf_xs": "https://drive.google.com/file/d/10ZVF3BzNNmNfDX5oPce_7SUZNwijqpvQ/view?usp=sharing",
+   # "gsv_xs": "https://drive.google.com/file/d/1ozI-r1V5sTvFaA-7UBIyfk0WvKfH0NRL/view?usp=sharing"
+#}
 #Nico
 #URLS = {
  #   "tokyo_xs": "https://drive.google.com/file/d/1AWCDdwwU4wHG7h1dbD8_j45uWGw4XuVd/view?usp=sharing",
   #  "sf_xs": "https://drive.google.com/file/d/1ZHji6-20BvwhlGA_XilIg2VOYFOvyUez/view?usp=sharing",
    # "gsv_xs": "https://drive.google.com/file/d/1Z-_7suk2_273hHE8COG9MLzRhDW8qCK7/view?usp=sharing"
 #}
+#Berton
+URLS = {
+    "tokyo_xs": "https://drive.google.com/file/d/15QB3VNKj93027UAQWv7pzFQO1JDCdZj2/view?usp=share_link",
+    "sf_xs": "https://drive.google.com/file/d/1tQqEyt3go3vMh4fj_LZrRcahoTbzzH-y/view?usp=share_link",
+    "gsv_xs": "https://drive.google.com/file/d/1q7usSe9_5xV5zTfN-1In4DlmF5ReyU_A/view?usp=share_link"
+}
 import os
 import gdown
 import shutil
