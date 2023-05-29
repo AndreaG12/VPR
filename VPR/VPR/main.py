d3b9d5ccdfc3495a539b3f4dc6e1bc75f119ca0c
AndreaG12 <80697707+AndreaG12@users.noreply.github.com>
2023-05-29 13:26:46 +0200

    Update main.py

--- VPR/VPR/main.py
+++ VPR/VPR/main.py
@@ -90,6 +90,7 @@ class LightningModel(pl.LightningModule):
         
         # Feed forward the batch to the model
         descriptors = self(images)  # Here we are calling the method forward that we defined above
+        print(descriptors.size(), type(descriptors))
         if  args.self_supervised_learning:
             descriptors = descriptors[::2]
             ref_descriptors = descriptors[1::2]
