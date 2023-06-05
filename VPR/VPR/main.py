3def4c3b4c9a531b3973ee4e45edce59c69aeeae
LucaCatalano13 <93945436+LucaCatalano13@users.noreply.github.com>
2023-06-05 18:07:24 +0200

    Update main.py

--- VPR/VPR/main.py
+++ VPR/VPR/main.py
@@ -77,6 +77,14 @@ class LightningModel(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         if args.self_supervised_learning  or args.soft_supervised_learning:
             images, _ = batch
+            print(images[0].shape)
+            trasformata = tfm.ToPILImage()
+            img1 = trasformata(images[0])
+            img2 = trasformata(images[0])
+            plt.imshow(img1)
+            plt.show(block = False)
+            plt.imshow(img2)
+            plt.show(block = False)
             num_places, num_images_per_place, C, H, W = images.shape
             images = images.view(num_places * num_images_per_place, C, H, W)
             
@@ -88,14 +96,6 @@ class LightningModel(pl.LightningModule):
             images = images.view(num_places * num_images_per_place, C, H, W)
             labels = labels.view(num_places * num_images_per_place)
         #print(type(images[0]))
-        print(images[0])
-        trasformata = tfm.ToPILImage()
-        img1 = trasformata(images[0])
-        img2 = trasformata(images[0])
-        plt.imshow(img1)
-        plt.show(block = False)
-        plt.imshow(img2)
-        plt.show(block = False)
         
         
         # Feed forward the batch to the model
