e5249a31f8476d7edd3ebea011603db9762be211
LucaCatalano13 <93945436+LucaCatalano13@users.noreply.github.com>
2023-06-05 19:37:52 +0200

    Update main.py

--- VPR/VPR/main.py
+++ VPR/VPR/main.py
@@ -81,7 +81,7 @@ class LightningModel(pl.LightningModule):
             #trasformata = tfm.ToPILImage()
             #img1 = trasformata(images[0][0])
             #img2 = trasformata(images[0][0])
-            plt.imshow(images[0][0].cpu().numpy().transpose(1,2,0).astype(np.uint8))
+            images[0][0].cpu().numpy().transpose(1,2,0).astype(np.uint8).img.save('pippo.jpg')
             plt.imshow(images[0][1].cpu().numpy().transpose(1,2,0).astype(np.uint8))
             plt.show()
             num_places, num_images_per_place, C, H, W = images.shape
