f36f3ee2398260fa1340a684a6cbc6b01352f813
AndreaG12 <80697707+AndreaG12@users.noreply.github.com>
2023-06-07 12:31:00 +0200

    Update train_dataset.py

--- VPR/VPR/datasets/train_dataset.py
+++ VPR/VPR/datasets/train_dataset.py
@@ -72,7 +72,7 @@ class TrainDataset(Dataset):
             customized_transform = tfm.Compose([
                 tfm.RandomHorizontalFlip(p = 0.5),
                 #tfm.RandomApply([tfm.ColorJitter(brightness = 0.5,  
-                                   contrast = 0.5, 
+                       #            contrast = 0.5, 
                  #                   saturation = 0.5,
                   #                  hue = 0.1)], p = 0.8) ,
                 tfm.RandomGrayscale(),
