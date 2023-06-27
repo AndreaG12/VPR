15ab296cf1674e2f4dd3cedc50ec3d6afb7e2143
AndreaG12 <80697707+AndreaG12@users.noreply.github.com>
2023-06-27 21:52:44 +0200

    Update train_dataset.py

--- VPR/VPR/datasets/train_dataset.py
+++ VPR/VPR/datasets/train_dataset.py
@@ -1,21 +1,39 @@
 import torch
 import numpy as np
 from glob import glob
-#from PIL import Image
+from PIL import Image
 from torch.utils.data import Dataset
 import torchvision.transforms as tfm
 from collections import defaultdict
-import matplotlib.pyplot as plt
-from PIL import Image, ImageOps, ImageFilter
-
-
-import sys
-import myparser
+import PIL
+from PIL import ImageOps, ImageFilter
 
 default_transform = tfm.Compose([
     tfm.ToTensor(),
     tfm.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
+
+class GaussianBlur(object):
+    def __init__(self, p):
+        self.p = p
+
+    def __call__(self, img):
+        if np.random.rand() < self.p:
+            sigma = np.random.rand() * 1.9 + 0.1
+            return img.filter(ImageFilter.GaussianBlur(sigma))
+        else:
+            return img
+
+
+class Solarization(object):
+    def __init__(self, p):
+        self.p = p
+
+    def __call__(self, img):
+        if np.random.rand() < self.p:
+            return ImageOps.solarize(img)
+        else:
+            return img
 
 class TrainDataset(Dataset):
     def __init__(
@@ -24,6 +42,7 @@ class TrainDataset(Dataset):
         img_per_place=4,
         min_img_per_place=4,
         transform=default_transform,
+        self_supervised=False
     ):
         super().__init__()
         self.dataset_folder = dataset_folder
@@ -39,6 +58,44 @@ class TrainDataset(Dataset):
             f"img_per_place should be less than {min_img_per_place}"
         self.img_per_place = img_per_place
         self.transform = transform
+        
+     
+        # self.transformer_aug = tfm.Compose([tfm.RandomResizedCrop(224, interpolation=tfm.InterpolationMode.BICUBIC),
+        #         tfm.RandomHorizontalFlip(p=0.5),
+        #         tfm.RandomApply([tfm.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1)], p=0.8,),
+        #         tfm.RandomGrayscale(p=0.2),
+        #         GaussianBlur(p=1.0),
+        #         Solarization(p=0.0),
+        #          tfm.ToTensor()])
+        
+        
+        self.transformer_aug = tfm.Compose([
+                    # tfm.RandomHorizontalFlip(p = 0.7),
+                #   tfm.RandomCrop((150, 150)),
+                    #tfm.ColorJitter(brightness = (0.1,0.9)) ,
+                    #tfm.RandomGrayscale(),
+                    
+                    # tfm.RandomApply(transforms=[
+                    #                             #tfm.RandomAffine(30, translate=(0.2,0.2), scale=None, shear=None, interpolation=tfm.InterpolationMode.NEAREST, fill=0, center=None),
+                    #                             # tfm.RandomEqualize(p=0.6),
+                    #                            # tfm.RandomPerspective(p=0.8, distortion_scale=0.6),
+                    #                            # tfm.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
+                    #                             
+                    #                             ],
+                    #                 p=1),
+                   
+                    # tfm.RandomApply(transforms=[
+                    #     #tfm.RandomHorizontalFlip(p = 0.7),
+                    #     tfm.ColorJitter(brightness = (0.1,0.9)) ,
+                    #     tfm.RandomCrop(size=224),
+                    # ], p=0.5),
+                    tfm.RandomHorizontalFlip(p = 1),
+                    tfm.RandomPerspective(p=0.5),
+                    tfm.RandomCrop(size=224),
+                    tfm.ToTensor(),
+                    tfm.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                ])
+        self.self_supervised = self_supervised
 
         # keep only places depicted by at least min_img_per_place images
         for place_id in list(self.dict_place_paths.keys()):
@@ -48,57 +105,37 @@ class TrainDataset(Dataset):
         self.places_ids = sorted(list(self.dict_place_paths.keys()))
         self.total_num_images = sum([len(paths) for paths in self.dict_place_paths.values()])
 
-    
-
     def __getitem__(self, index):
-        
-        args = myparser.parse_arguments() 
         place_id = self.places_ids[index]
         all_paths_from_place_id = self.dict_place_paths[place_id]
         chosen_paths = np.random.choice(all_paths_from_place_id, self.img_per_place)
         images = [Image.open(path).convert('RGB') for path in chosen_paths]
-
-        customized_transform2 = tfm.Compose([
-                    tfm.RandomHorizontalFlip(p = 1),
-                  #  tfm.ColorJitter(brightness = (0.1,0.9)) ,
-                  #  tfm.RandomGrayscale(),
-                    
-                   # tfm.RandomApply(transforms=[
-                                                #tfm.RandomAffine(30, translate=(0.2,0.2), scale=None, shear=None, interpolation=tfm.InterpolationMode.NEAREST, fill=0, center=None),
-                         #                       tfm.RandomEqualize(p=0.6),
-                  #  tfm.RandomPerspective(p=0.5),
-                                               # tfm.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
-                   # tfm.RandomPerspective(p=0.5),
-                    #tfm.RandomCrop(size=224),
-                    #tfm.RandomHorizontalFlip(p = 1),
-                    tfm.RandomPerspective(p=0.5),
-                    #tfm.RandomHorizontalFlip(p = 1),
-                    tfm.RandomCrop(size=224),
-                    #tfm.RandomPerspective(p=0.5),
-                    #                            ],
-                     #               p=1),
-                    tfm.ToTensor(),
-                    tfm.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-                ])
-        if args.self_supervised_learning:
-            
-            #image = Image.open(chosen_paths[0]).convert('RGB')  #code line to highlight the self-sup. approach
-            img = self.transform(images[0])
-         
-            augmImg = customized_transform2(images[0])
-            
-            return torch.stack((img, augmImg)), torch.tensor(index).repeat(2)   #number of final augmented images
+       
+        tfm_images = [self.transform(img) for img in images]
         
-        if args.soft_supervised_learning:
-            #images= Image.open((chosen_paths[0], chosen_paths[1])).convert('RGB')
-            image1 = self.transform(images[0])
-            image2 = customized_transform2(images[1])
-            #image = [self.transform((image1, image2)) for img in images]
-            return torch.stack((image1, image2)), torch.tensor(index).repeat(2)
-        else:
-            images = [Image.open(path).convert('RGB') for path in chosen_paths]
-            images = [self.transform(img) for img in images]
-            return torch.stack(images), torch.tensor(index).repeat(self.img_per_place)
+        
+        if self.self_supervised:
+            image = images[0]
+            images_aug = images[1:]
+            image = self.transform(image)
+            images_aug = [self.transform(img) for img in images_aug]
+            #image = [image for _ in range(3)]
+
+            #return torch.stack(tfm_images), torch.stack(images_aug), torch.tensor(index).repeat(self.img_per_place)
+           
+            #print(f"\niamge: {image.shape}, aug: {images_aug[0].shape}, {len(images_aug)}\n")
+            new_images = [image] + images_aug 
+            return torch.stack(new_images), torch.tensor(index).repeat(self.img_per_place)
+           
+            # tfm_images = [self.transform(img) for img in images]
+            # augImages= [self.transformer_aug(img) for img in images]
+
+            # final_images= [img for pair in zip(tfm_images, augImages) for img in pair]
+            
+            # return torch.stack(final_images), torch.tensor(index).repeat(2* self.img_per_place)
+        
+        return torch.stack(tfm_images), torch.tensor(index).repeat(self.img_per_place)
+    
 
     def __len__(self):
         """Denotes the total number of places (not images)"""
